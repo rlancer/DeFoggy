beb24416d3af49884bb087a736cc94b10ac0a52c
Robert Lancer <rlancer@attentivemobile.com>
2021-12-28 23:55:50 -0500

    added chakra

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ChakraProvider>
+      <App />
+      </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +18,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
